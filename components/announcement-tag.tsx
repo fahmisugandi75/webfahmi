cb87ddf4543f4b0f5cfda582a4b4f518b39f06f2
fahmisugandi75 <hello@fahmisugandi.com>
2024-10-07 02:25:15 +0700

    Update announcement-tag.tsx

--- components/announcement-tag.tsx
+++ components/announcement-tag.tsx
@@ -29,9 +29,9 @@ export default function Announcement() {
           onClick={handleClick}
         >
           <BorderBeam />
-          <span className="relative flex h-3 w-3 mr-2">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-orange-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-orange-500"></span>
+          <span className="relative flex h-3 w-3 mr-3 items-center justify-center">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
           </span>
           I am available to work.{' '}
         </div>
