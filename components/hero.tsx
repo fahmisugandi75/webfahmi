0306863628e8bb035a6894a055263c65200bffa8
fahmisugandi75 <hello@fahmisugandi.com>
2024-10-07 01:53:02 +0700

    last

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,6 +4,7 @@ import Announcement from '@/components/announcement-tag'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import CalendarBook from './calendar-book'
+import IconCloud from '@/components/ui/icon-cloud'
 
 export default function Hero() {
   return (
@@ -19,13 +20,13 @@ export default function Hero() {
       <div className="relative z-10 h-full flex items-center w-full px-6 lg:px-8">
         <div className="mx-auto max-w-7xl w-full">
           <div className="flex flex-col lg:flex-row items-center justify-between">
-            <div className="lg:w-1/2 text-center lg:text-left">
+            <div className="lg:w-1/2 text-center lg:text-left mb-16 lg:-mb-16">
               <Announcement />
               <h1 className="text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:text-7xl my-2">
                 Senior UX/UI designer for hire!
               </h1>
-              <p className="mt-4 text-base sm:text-lg leading-7 text-gray-600">
-                Fahmi is a seasoned expert in graphic design, UI/UX, and web development (WordPress, PHP, NextJs), with over 10 years of experience.
+              <p className="mt-8 text-base sm:text-lg leading-7 text-gray-600">
+                Fahmi is a seasoned expert in graphic design, UI/UX, and web development (WordPress, PHP, NextJs), with over 10 years of experience for international clients across consulting, banking, healthcare, wellness, and tech industries.
               </p>
               <div className="mt-6 sm:mt-10 flex items-center justify-center lg:justify-start gap-x-6">
                 <Link href="https://yfxskljlopmcwmghykcl.supabase.co/storage/v1/object/public/avatars/Fahmi%20-%20Senior%20Graphic%20&%20Web%20Designer%20-%20copy.pdf" target="_blank">
@@ -36,14 +37,18 @@ export default function Hero() {
                 <CalendarBook />
               </div>
             </div>
-            <div className="lg:w-1/2 mt-6 lg:mt-0 flex justify-center">
+            <div className="lg:w-1/2 mt-6 lg:mt-0 flex justify-center flex-col">
+            <div className="-mt-14">
+                <IconCloud iconSlugs={['react', 'nextjs', 'typescript', 'tailwindcss', 'figma', 'adobephotoshop', 'wordpress', 'adobeillustrator', 'html5', 'css3', 'supabase', 'github', 'elementor', 'adobeindesign', 'amazonaws']} />
+              </div> 
               <Image
                 src="https://yfxskljlopmcwmghykcl.supabase.co/storage/v1/object/public/avatars/84b3ff40-57d8-40fa-9708-539e129a3f57-0.6619754556250563.png"
                 alt="Fahmi - Senior Website Designer"
-                width={600}
-                height={600}
-                className="-mr-32"
+                width={1000}
+                height={1000}
+                className="-mr-96 -mt-96 z-10"
               />
+              
             </div>
           </div>
         </div>
