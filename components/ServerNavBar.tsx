be2c4d57bba624671eaa87c81ef4899c1ce97546
fahmisugandi75 <hello@fahmisugandi.com>
2024-10-06 17:14:52 +0700

    Delete ServerNavBar.tsx

--- components/ServerNavBar.tsx
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers';
-import { ClientNavBar } from './ClientNavBar';
-
-export function ServerNavBar() {
-  const cookieStore = cookies();
-  const hasEnvVars = !!cookieStore.get('hasEnvVars');
-
-  return <ClientNavBar hasEnvVars={hasEnvVars} />;
-}
