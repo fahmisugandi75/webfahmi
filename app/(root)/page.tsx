95b5e5ba0bc5930a89b30093bbcfe1234f87ea26
fahmisugandi75 <hello@fahmisugandi.com>
2024-10-07 02:11:18 +0700

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,10 @@
 import Hero from '@/components/hero'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Senior UX/UI Designer for Hire',
+  description: 'Fahmi is a seasoned expert in graphic design, UI/UX, and web development (WordPress, PHP, NextJs), with over 10 years of experience for international clients across consulting, banking, healthcare, wellness, and tech industries.',
+}
 
 export default function Home() {
   return (
